179dcd35d898dad8f71ab69033c48e30b8eaaac7
davlatsultonov <davlatsultonov2001@gmail.com>
2022-05-09 18:21:16 +0500

    project init

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: './dist/'
+        }
+    });
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]))
+        .pipe(dest('dist/images'))
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/owl.carousel/dist/owl.carousel.min.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(dest('dist/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream())
+}
+
+function html() {
+    return src('app/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist'))
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+    ], { base: 'app' })
+        .pipe(dest('dist'))
+}
+
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/**/*.html']).on('change', series(html, browserSync.reload))
+}
+
+exports.html = html;
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, html, build);
+exports.default = parallel(html, styles, scripts, browsersync, watching)
